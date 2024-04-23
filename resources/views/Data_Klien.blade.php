057f066131f0e03aa17a0989098a9f77600202f8
yolaseptia <yolaseptia886@gmail.com>
2024-04-23 20:51:18 +0700

    gakpenting

---
+++ resources/views/Data_Klien.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rekam Medis Pasien - RS Rachma Husada</title>
+    <link rel="stylesheet" href="/lp/css/Klien.css">
+</head>
+<body>
+    <main>
+        <section class="data-pasien">
+            <h2>Selamat Datang, Bagas!</h2>
+        </section>
+        <br>
+        <section class="keterangan">
+            <h2>Pasien Rawat Jalan</h2>
+            <div class="cetak_pdf">
+                <button class="PDF" type="button">PDF</button>
+                <button class="Cetak" type="button">Cetak</button> 
+            </div>
+            <div class="tambah-pasien">
+                <button type="button">Tambah Pasien</button>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nama</th>
+                        <th>Nama Wali</th>
+                        <th>Alamat</th>
+                        <th>No.Telepon</th>
+                        <th>Paket</th>
+                        <th>L/P</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Rini Febria</td>
+                        <td> Viar Putri</td>
+                        <td>Magetan</td>
+                        <td>0896728392</td>
+                        <td>1</td>
+                        <td>P</td>
+                        <td>
+                            <button type="button">Edit</button>
+                            <button type="button">Hapus</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Siti Anissa</td>
+                        <td>Putri Silvia</td>
+                        <td>Madiun</td>
+                        <td>0872273673</td>
+                        <td>4</td>
+                        <td>P</td>
+                        <td> <button type="button">Edit</button>
+                            <button type="button">Hapus</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Andika Putra</td>
+                        <td>Wahyu Mukti</td>
+                        <td>Ponorogo</td>
+                        <td>08523556277</td>
+                        <td>1</td>
+                        <td>L</td>
+                        <td>
+                            <button type="button">Edit</button>
+                            <button type="button">Hapus</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Risna Triayu</td>
+                        <td>Shelly Lintang</td>
+                        <td>Ngawi</td>
+                        <td>08128863527</td>
+                        <td>3</td>
+                        <td>P</td>
+                        <td>
+                            <button type="button">Edit</button>
+                            <button type="button">Hapus</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Rendy Catur</td>
+                        <td>Surya Ilhami</td>
+                        <td>Madiun</td>
+                        <td>0821536622</td>
+                        <td>2</td>
+                        <td>L</td>
+                        <td>
+                            <button type="button">Edit</button>
+                            <button type="button">Hapus</button>
+                        </td>
+        </section>
+    </main>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminFormController;
+use App\Http\Controllers\KlienController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,6 +12,8 @@ Route::get('/', function () {
 Route::get('home', [HomeController::class, 'index'])->name('home');
 
 Route::get('login', [HomeController::class, 'login'])->name('login');
+
+Route::get('/Data_Klien', [KlienController::class, 'index']);
 
 // Route form Controller 
 
